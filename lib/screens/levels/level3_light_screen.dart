8870f2a3df8c4b229ddb4e21bfd6a01924faf878
Austo1 <simiyuaustine14@gmail.com>
2026-08-24 21:10:58 +0300

    feat: add level 3 screen

---
+++ lib/screens/levels/level3_light_screen.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+class Level3LightScreen extends StatelessWidget { const Level3LightScreen({super.key}); @override Widget build(BuildContext c)=>const Scaffold(body: Center(child: Text('Level 3 — LIGHT • 10/20 Ksh'))); }
