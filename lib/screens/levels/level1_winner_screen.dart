9aa023dedb1eec74fea55bb3d0d58095c35018b7
Austo1 <simiyuaustine14@gmail.com>
2026-08-24 21:10:52 +0300

    feat: add level 1 screen

---
+++ lib/screens/levels/level1_winner_screen.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+class Level1WinnerScreen extends StatelessWidget { const Level1WinnerScreen({super.key}); @override Widget build(BuildContext c)=>const Scaffold(body: Center(child: Text('Level 1 — PRO Winner • 50 Ksh'))); }
