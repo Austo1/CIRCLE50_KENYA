315e293f03fcbc9bd7e05f67a0b6de465b82352b
Austo1 <simiyuaustine14@gmail.com>
2026-08-24 21:10:55 +0300

    feat: add level 2 screen

---
+++ lib/screens/levels/level2_supporter_screen.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart';
+class Level2SupporterScreen extends StatelessWidget { const Level2SupporterScreen({super.key}); @override Widget build(BuildContext c)=>const Scaffold(body: Center(child: Text('Level 2 — Supporter • 50 Ksh'))); }
